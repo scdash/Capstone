c26f242a0b6703febe149f8e8294104c3b5135d2
Steven Dash <scdash91@outlook.com>
2018-03-06 11:34:21 -0500

    Created Widget GUI
    Made basic GUI which will have the script attatched

---
+++ Assets/Editor/MoveEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+//[InitializeOnLoad]
+public class ActionEditor : EditorWindow {
+    string string1 = "Insert Here";
+    bool groupEnabled;
+    bool move = true;
+    float weight = 4.2f;
+
+    static ActionEditor() {
+        Debug.Log("ActionEditor is up and running");
+        ShowWindow();
+    }
+
+    [MenuItem("Window/Action Editor")]
+    public static void ShowWindow() {
+        EditorWindow.GetWindow(typeof(ActionEditor));
+    }
+
+    void OnGUI() {
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+        string1 = EditorGUILayout.TextField("Rule", string1);
+        groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
+            move = EditorGUILayout.Toggle("Movement", move);
+            weight = EditorGUILayout.Slider("Action Weight", weight, 0, 10);
+        EditorGUILayout.EndToggleGroup();
+        //add = GUILayout.Button("Add Action");
+    }
+}
