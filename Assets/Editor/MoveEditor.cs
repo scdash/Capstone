0c47dee162354ce3c963bbfe7bd9af6f455b888e
Steven Dash <scdash91@outlook.com>
2018-03-29 10:31:02 -0400

    Added to the Animation Editor
    Gave the Animation Editor more capability and added a weight slider

--- Assets/Editor/MoveEditor.cs
+++ Assets/Editor/MoveEditor.cs
@@ -13,6 +13,7 @@ public class ActionEditor : EditorWindow {
     public AnimationClip jumpAnim;
     public AnimationClip moveAnim;
     public string moveName;
+    public float moveWeight = 5.00f;
 
     [MenuItem("Window/Action Editor")]
     static void OpenWindow() {
@@ -40,8 +41,9 @@ public class ActionEditor : EditorWindow {
         }
 
         GUILayout.Label("Add Move", EditorStyles.boldLabel);
-        moveName = EditorGUILayout.TextField("Animator ID", "Move Name");
-        moveAnim = EditorGUILayout.ObjectField("Move Animation", moveAnim, typeof(AnimationClip), true) as AnimationClip;
+        moveName = EditorGUILayout.TextField("Action ID", "Move Name");
+        moveAnim = EditorGUILayout.ObjectField("Action Animation", moveAnim, typeof(AnimationClip), true) as AnimationClip;
+        moveWeight = EditorGUILayout.Slider("Action Weight", moveWeight, 0, 10);
 
         GUILayout.Label("Delete Move", EditorStyles.boldLabel);
     }
@@ -50,8 +52,35 @@ public class ActionEditor : EditorWindow {
     {
         AnimatorController controller = AnimatorController.CreateAnimatorControllerAtPath("Assets/Scripts/" + targetM.name + ".controller");
         controller.AddParameter("Speed", AnimatorControllerParameterType.Float);
+
         AnimatorState idleState = controller.layers[0].stateMachine.AddState("Idle");
         idleState.motion = idleAnim;
+
+        BlendTree blendTree1;
+        BlendTree blendTree2;
+        AnimatorState moveState1 = controller.CreateBlendTreeInController("Move Forward", out blendTree1);
+        AnimatorState moveState2 = controller.CreateBlendTreeInController("Move Backward", out blendTree2);
+
+        blendTree1.blendType = BlendTreeType.Simple1D;
+        blendTree2.blendType = BlendTreeType.Simple1D;
+        blendTree1.blendParameter = "Speed";
+        blendTree2.blendParameter = "Speed";
+        blendTree1.AddChild(walkFore);
+        blendTree1.AddChild(runFore);
+        blendTree2.AddChild(walkBack);
+        blendTree2.AddChild(runBack);
+
+        AnimatorStateTransition leaveIFore = idleState.AddTransition(moveState1);
+        AnimatorStateTransition leaveMFore = moveState1.AddTransition(idleState);
+        AnimatorStateTransition leaveIBack = idleState.AddTransition(moveState2);
+        AnimatorStateTransition leaveMBack = moveState2.AddTransition(idleState);
+
+        leaveIFore.AddCondition(AnimatorConditionMode.Greater, 0.01f, "Speed");
+        leaveMFore.AddCondition(AnimatorConditionMode.Less, 0.01f, "Speed");
+        leaveIBack.AddCondition(AnimatorConditionMode.Less, -0.01f, "Speed");
+        leaveMBack.AddCondition(AnimatorConditionMode.Greater, -0.01f, "Speed");
+
+        targetM.GetComponent<Animator>().runtimeAnimatorController = controller;
     }
 }
 #endif
